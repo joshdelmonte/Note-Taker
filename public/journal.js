b91e1246dcca195c95b3f62de8da9ca642d96286
joshdelmonte <joshdelmonte@gmail.com>
2022-10-31 00:40:45 -0600

    had to change const to let for notes

--- public/journal.js
+++ public/journal.js
@@ -2,8 +2,8 @@ const exp = require("constants");
 const { response } = require("express");
 const express = require(`express`);
 const rua = require(`path`);
-const notes = require(`./db/db.json`)
-const {uid} = require(`uid`)
+let notes = require(`./db/db.json`);
+const {uid} = require(`uid`);
 const PORT = 3005;
 
 const app = express();
@@ -11,22 +11,22 @@ const app = express();
 // Set route to access html files
 app.use(express.static(rua.join(__dirname, `public`)));
 app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.json());
 
 // Set path for indx
 app.get(`/`, (req, res) => {
     res.sendFile(rua.join(__dirname, `public`, 'index.html'))
-})
+});
 
 // Set path for notes
 app.get(`/notes`, (req, res) => {
     res.sendFile(rua.join(__dirname, `public`, 'notes.html'))
-})
+});
 
 //Designate notes pattern and layout from api w/ this data
 app.get(`/api/notes`, (req, res) => {
     res.json(notes)
-})
+});
 
 //Apply this output from afore data
 app.post(`/api/notes`, (req, res) => {
@@ -34,15 +34,16 @@ app.post(`/api/notes`, (req, res) => {
         titulo:req.body.title,
         identidade: uid(),
         escrita:req.body.text 
-    }
+    };
 
-    notes.push(notasNovo)
-    response.json(200)
+    notes.push(notasNovo);
+    response.json(200);
 })
 
 //Designate code blocks for DELETE (`DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete.)
 app.delete(`/api/notes/:id`, (req, res) =>{
-    notes = notes.filter(note => note.identidade != req.params.id)
-})
+    notes = notes.filter(note => note.identidade !== req.params.identidade);
+    res.json(notes)
+});
 
 app.listen(proccess.env.PORT || 3005);
